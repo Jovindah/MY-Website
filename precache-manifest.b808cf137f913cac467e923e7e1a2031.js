4909a5bcc8a26fb1bc71e990c8fe67ee0ccec64d
Umer-Naseer-Jovindah <99118963+Umer-Naseer-Jovindah@users.noreply.github.com>
2022-06-05 13:39:17 +0200

    Updates

---
+++ precache-manifest.b808cf137f913cac467e923e7e1a2031.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b53ccb2e090ed45cae8c",
+    "url": "/MY-Website/static/css/main.1d73edf3.chunk.css"
+  },
+  {
+    "revision": "b53ccb2e090ed45cae8c",
+    "url": "/MY-Website/static/js/main.b53ccb2e.chunk.js"
+  },
+  {
+    "revision": "ab20aff2a64a06a7bd59",
+    "url": "/MY-Website/static/css/1.6baba45a.chunk.css"
+  },
+  {
+    "revision": "ab20aff2a64a06a7bd59",
+    "url": "/MY-Website/static/js/1.ab20aff2.chunk.js"
+  },
+  {
+    "revision": "7b3598c7acb06e0ef3f2",
+    "url": "/MY-Website/static/js/runtime~main.7b3598c7.js"
+  },
+  {
+    "revision": "45eb1fb8e450da55984796ad1b06e769",
+    "url": "/MY-Website/index.html"
+  }
+];
